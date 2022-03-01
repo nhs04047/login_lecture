78362bdbe2fa86d021889a24f999e2de79a18d94
보현 김 <nhs04047@gmail.com>
2022-03-01 10:17:03 +0900

    .

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@
 const express = require("express")
 const app = express()
 
-const home = require("./routes/home")
+const home = require("./src/publice/js/home")
 
-app.set("views", "./views")
+app.set("views", "./src/views")
 app.set("view engine", "pug")
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/",home)
 
